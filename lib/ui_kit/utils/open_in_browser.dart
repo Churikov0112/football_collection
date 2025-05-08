d138a6639c4a7dd6907930205010d64845445973
Egor Churikov <churikov.egor@bk.ru>
2025-05-08 13:21:40 +0400

    fix: link to developer website

--- lib/ui_kit/utils/open_in_browser.dart
+++ lib/ui_kit/utils/open_in_browser.dart
@@ -1,12 +1,21 @@
+import 'package:football_collection/services/toast/toast_service.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 Future<void> openInBrowser(String link) async {
   final url = Uri.parse(link);
-  final canLaunch = await canLaunchUrl(url);
-  if (canLaunch) {
+  try {
     await launchUrl(
       url,
       mode: LaunchMode.externalApplication,
     );
+  } catch (e) {
+    ToastService.showToast(title: "Произошла ошибка!", subtitle: e.toString());
   }
+  // final canLaunch = await canLaunchUrl(url);
+  // if (canLaunch) {
+  //   await launchUrl(
+  //     url,
+  //     mode: LaunchMode.externalApplication,
+  //   );
+  // }
 }
