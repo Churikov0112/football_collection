ee145c8f80c265e4197d2835518f497c4ace9189
Egor Churikov <churikov.egor@bk.ru>
2026-04-07 13:57:37 +0300

    bump

--- lib/features/settings/presentation/ui/screens/settings_screen/settings_screen.dart
+++ lib/features/settings/presentation/ui/screens/settings_screen/settings_screen.dart
@@ -62,7 +62,7 @@ class SettingsScreen extends StatelessWidget {
                             presenter.toggleEnableConfetti(val);
                           },
                         ),
-                        ListTile(title: Text("v. 2.0.1+23")), // CHANGE VERSION
+                        ListTile(title: Text("v. 2.2.4+24")), // CHANGE VERSION
                       ],
                     ),
                     // Positioned(
