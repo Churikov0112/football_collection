78625b1777a1a3dd81eb94c17bee60bdbdfc8d7c
Egor Churikov <churikov.egor@bk.ru>
2026-04-08 21:29:12 +0300

    feat: album players sorting

--- lib/features/football_cards/presentation/screens/album_screen/widgets/cards_list.dart
+++ lib/features/football_cards/presentation/screens/album_screen/widgets/cards_list.dart
@@ -1,5 +1,28 @@
 part of '../football_players_album_screen.dart';
 
+// Где-то в утилитах или рядом с footballPlayerPositionToShort
+const Map<String, int> _footballPlayerPositionOrder = {
+  "Goalkeeper": 0,
+  "Centre-Back": 1,
+  "Left-Back": 2,
+  "Right-Back": 3,
+  "Defensive Midfield": 4,
+  "Central Midfield": 5,
+  "Attacking Midfield": 6,
+  "Left Midfield": 7,
+  "Right Midfield": 8,
+  "Left Winger": 9,
+  "Right Winger": 10,
+  "Centre-Forward": 11,
+  "Second Striker": 12,
+  "Striker": 13,
+};
+
+int _getFootballPlayerPositionOrder(String? position) {
+  if (position == null) return 99;
+  return _footballPlayerPositionOrder[position] ?? 99;
+}
+
 class _FootballCardsList extends StatelessWidget {
   const _FootballCardsList({required this.country});
 
@@ -24,18 +47,24 @@ class _FootballCardsList extends StatelessWidget {
       builder: (context, allFootballCardsState) {
         final cards = allFootballCardsState.data ?? [];
 
-        // эмблема выводится выводится первой, тренер вторым, потом игроки в следующем порядке
-        // вратари
-        // защитники
-        // полузащитники
-        // нападающие
-        // в конце выводятся легенды
-
         final sortedCards = List<CardModel>.from(cards)
           ..sort((a, b) {
+            // Сначала сортируем по типу карты
             final priorityA = _getPriority(a);
             final priorityB = _getPriority(b);
-            return priorityA.compareTo(priorityB);
+
+            if (priorityA != priorityB) {
+              return priorityA.compareTo(priorityB);
+            }
+
+            // Если оба - игроки, сортируем по позиции
+            if (a is FootballPlayerCardModel && b is FootballPlayerCardModel) {
+              final positionOrderA = _getFootballPlayerPositionOrder(a.position);
+              final positionOrderB = _getFootballPlayerPositionOrder(b.position);
+              return positionOrderA.compareTo(positionOrderB);
+            }
+
+            return 0;
           });
 
         return Expanded(
