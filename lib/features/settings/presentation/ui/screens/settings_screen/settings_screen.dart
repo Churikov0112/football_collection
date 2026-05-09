37d26fae363f882c41d71a39e9a4c6862c309f4b
Egor Churikov <churikov.egor@bk.ru>
2026-05-09 19:49:02 +0300

    fix: add NSPhotoLibraryUsageDescription

--- lib/features/settings/presentation/ui/screens/settings_screen/settings_screen.dart
+++ lib/features/settings/presentation/ui/screens/settings_screen/settings_screen.dart
@@ -96,7 +96,7 @@ class SettingsScreen extends StatelessWidget {
                         //     presenter.changeTrackAutoRotateSpeed(val);
                         //   },
                         // ),
-                        const ListTile(title: Text("v. 2.2.11+31")), // CHANGE VERSION
+                        const ListTile(title: Text("v. 2.2.12+32")), // CHANGE VERSION
                       ],
                     ),
                     // Positioned(
